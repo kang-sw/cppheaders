9769a647909595a38f2abf3df31e94195760d67b
kangsw <ki6080@gmail.com>
2021-11-21 01:42:26 +0900

    Remove array constructor which invalidates implicit constructor {}

--- hasher.hxx
+++ hasher.hxx
@@ -65,9 +65,6 @@ struct basic_key {
   template <typename Ty_>
   constexpr static basic_key create(Ty_&& r) noexcept { return {hasher::fnv1a_64(r)}; }
 
-  template <size_t N_>
-  constexpr basic_key(char const (&str)[N_]) noexcept : value(hasher::fnv1a_64(str, str + N_)) {}
-
  public:
   uint64_t value = {};
 };
