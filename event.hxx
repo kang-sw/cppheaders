4855d4180ef6b330a4960a178bc3c028bce2f52f
kangsw <ki6080@gmail.com>
2021-11-23 20:31:45 +0900

    Change class name

--- event.hxx
+++ event.hxx
@@ -10,49 +10,23 @@
 #include "__namespace__.h"
 
 namespace CPPHEADERS_NS_ {
-template <typename Mutex_>
-struct lock_guard {
-  enum { nothrow_lockable = std::is_nothrow_invocable_v<decltype(&Mutex_::lock), Mutex_*> };
-  enum { nothrow_unlockable = std::is_nothrow_invocable_v<decltype(&Mutex_::unlock), Mutex_*> };
+using event_key = basic_key<class LABEL_delegate_key>;
 
-  lock_guard(Mutex_& mtx) noexcept(nothrow_lockable) : _ref(mtx) {
-    _ref.lock();
-  }
-
-  void lock() noexcept(nothrow_lockable) {
-    _ref.lock();
-    _locked = true;
-  }
-
-  void unlock() noexcept(nothrow_unlockable) {
-    _ref.unlock();
-    _locked = false;
-  }
-
-  ~lock_guard() noexcept(nothrow_unlockable) {
-    if (_locked)
-      _ref.unlock();
-  }
-
-  bool _locked = true;
-  Mutex_& _ref;
-};
-
-using delegate_key = basic_key<class LABEL_delegate_key>;
-
-enum class delegate_invoke_result {
+enum class event_control {
   ok      = 0,
   expire  = 1,
   consume = 2,
 };
 
-inline delegate_invoke_result operator|(delegate_invoke_result a, delegate_invoke_result b) {
-  return (delegate_invoke_result)(int(a) | int(b));
+inline event_control operator|(event_control a, event_control b) {
+  return (event_control)(int(a) | int(b));
 }
 
-enum { DELEGATE_BITS = 61 };
+enum {
+  DELEGATE_BITS = 61
+};
 
-enum class delegate_priority : uint64_t {
+enum class event_priority : uint64_t {
   last      = 0,
   very_low  = 1ull << DELEGATE_BITS,
   low       = 2ull << DELEGATE_BITS,
@@ -63,12 +37,12 @@ enum class delegate_priority : uint64_t {
 };
 
 template <typename Mutex_, typename... Args_>
-class basic_delegate {
+class basic_event {
  public:
-  using event_fn = std::function<delegate_invoke_result(Args_...)>;
+  using event_fn = std::function<event_control(Args_...)>;
 
   struct _entity_type {
-    delegate_key id;
+    event_key id;
     event_fn function;
     uint64_t priority = 0;
 
@@ -92,11 +66,11 @@ class basic_delegate {
 
  public:
   class handle {
-    friend class basic_delegate;
-    basic_delegate* owner_ = {};
-    delegate_key key_      = {};
+    friend class basic_event;
+    basic_event* owner_ = {};
+    event_key key_      = {};
 
-    handle(basic_delegate* o, delegate_key key) noexcept
+    handle(basic_event* o, event_key key) noexcept
             : owner_{o}, key_{key} {}
 
    public:
@@ -114,15 +88,17 @@ class basic_delegate {
   };
 
  public:
-  basic_delegate() noexcept = default;
-  basic_delegate(basic_delegate&& rhs) noexcept { *this = std::move(rhs); }
-  basic_delegate(basic_delegate const& rhs) noexcept { *this = rhs; }
-  basic_delegate& operator=(basic_delegate&& rhs) noexcept {
+  basic_event() noexcept = default;
+  basic_event(basic_event&& rhs) noexcept { *this = std::move(rhs); }
+  basic_event(basic_event const& rhs) noexcept { *this = rhs; }
+
+  basic_event& operator=(basic_event&& rhs) noexcept {
     lock_guard a{_mtx}, b{rhs._mtx};
     _events = std::move(rhs._events);
     _mtx.unlock(), rhs._mtx.unlock();
   }
-  basic_delegate& operator=(basic_delegate const& rhs) noexcept {
+
+  basic_event& operator=(basic_event const& rhs) noexcept {
     lock_guard a{_mtx}, b{rhs._mtx};
     _events = rhs._events;
     _mtx.unlock(), rhs._mtx.unlock();
@@ -145,12 +121,12 @@ class basic_delegate {
         auto invoke_result = (int)it->function(std::forward<FnArgs_>(args)...);
         lock.lock();
 
-        if (invoke_result & (int)delegate_invoke_result::expire)
+        if (invoke_result & (int)event_control::expire)
           _events.erase(it++);
         else
           ++it;
 
-        if (invoke_result & (int)delegate_invoke_result::consume)
+        if (invoke_result & (int)event_control::consume)
           break;
       }
     }
@@ -158,8 +134,8 @@ class basic_delegate {
 
   template <typename Callable_>
   handle add(Callable_&& fn,
-             delegate_priority priority = delegate_priority::last,
-             uint64_t value             = 0) {
+             event_priority priority = event_priority::last,
+             uint64_t value          = 0) {
     lock_guard _{_mtx};
     auto* evt     = &_events.emplace_back();
     evt->id       = {++_hash_gen};
@@ -167,27 +143,21 @@ class basic_delegate {
 
     _dirty |= evt->priority != 0;
 
-    if constexpr (std::is_invocable_r_v<delegate_invoke_result, Callable_, Args_...>) {
+    if constexpr (std::is_invocable_r_v<event_control, Callable_, Args_...>) {
       evt->function = (std::forward<Callable_>(fn));
     } else if constexpr (std::is_invocable_r_v<bool, Callable_, Args_...>) {
-      evt->function = ([_fn = std::forward<Callable_>(fn)](auto&&... args) {
-        return _fn(args...) ? delegate_invoke_result::ok
-                            : delegate_invoke_result::expire;
-      });
+      evt->function = ([_fn = std::forward<Callable_>(fn)](auto&&... args) { return _fn(args...) ? event_control::ok
+                                                                                                 : event_control::expire; });
     } else if constexpr (std::is_invocable_v<Callable_>) {
-      evt->function = ([_fn = std::forward<Callable_>(fn)](auto&&... args) {
-        return _fn(), delegate_invoke_result::ok;
-      });
+      evt->function = ([_fn = std::forward<Callable_>(fn)](auto&&... args) { return _fn(), event_control::ok; });
     } else {
-      evt->function = ([_fn = std::forward<Callable_>(fn)](auto&&... args) {
-        return _fn(args...), delegate_invoke_result::ok;
-      });
+      evt->function = ([_fn = std::forward<Callable_>(fn)](auto&&... args) { return _fn(args...), event_control::ok; });
     }
 
     return handle{this, evt->id};
   }
 
-  void priority(handle const& h, delegate_priority offset, uint64_t value = 0) noexcept {
+  void priority(handle const& h, event_priority offset, uint64_t value = 0) noexcept {
     lock_guard _{_mtx};
     auto entity = _find(h);
     if (not entity)
@@ -235,8 +205,11 @@ class basic_delegate {
     if (not h.key_)
       throw std::logic_error{"invalid handle!"};
 
-    auto it = std::find_if(_events.begin(), _events.end(),
-                           [&](_entity_type const& s) { return h.key_ == s.id; });
+    auto it = std::find_if(
+            _events.begin(), _events.end(),
+            [&](_entity_type const& s) {
+              return h.key_ == s.id;
+            });
     return it == _events.end() ? nullptr : &*it;
   }
 
@@ -255,9 +228,9 @@ struct null_mutex {
 };
 
 template <typename... Args_>
-using delegate = basic_delegate<spinlock, Args_...>;
+using event = basic_event<spinlock, Args_...>;
 
 template <typename... Args_>
-using delegate_st = basic_delegate<null_mutex, Args_...>;
+using event_st = basic_event<null_mutex, Args_...>;
 
 }  // namespace CPPHEADERS_NS_
--- spinlock.hxx
+++ spinlock.hxx
@@ -14,6 +14,35 @@
 #include "__namespace__.h"
 
 namespace CPPHEADERS_NS_ {
+// lock guard utility
+template <typename Mutex_>
+struct lock_guard {
+  enum { nothrow_lockable = std::is_nothrow_invocable_v<decltype(&Mutex_::lock), Mutex_*> };
+  enum { nothrow_unlockable = std::is_nothrow_invocable_v<decltype(&Mutex_::unlock), Mutex_*> };
+
+  lock_guard(Mutex_& mtx) noexcept(nothrow_lockable) : _ref(mtx) {
+    _ref.lock();
+  }
+
+  void lock() noexcept(nothrow_lockable) {
+    _ref.lock();
+    _locked = true;
+  }
+
+  void unlock() noexcept(nothrow_unlockable) {
+    _ref.unlock();
+    _locked = false;
+  }
+
+  ~lock_guard() noexcept(nothrow_unlockable) {
+    if (_locked)
+      _ref.unlock();
+  }
+
+  bool _locked = true;
+  Mutex_& _ref;
+};
+
 //! @see https://rigtorp.se/spinlock/
 //! Applied slight modification to use atomic_flag
 struct spinlock {
