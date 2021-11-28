a0cd2fb0ed7c1a3ce8cdd0c655768cac33c2e094
kangsw <ki6080@gmail.com>
2021-11-28 13:46:17 +0900

    Fix json macro's line substitution

--- helper/nlohmann_json_macros.hxx
+++ helper/nlohmann_json_macros.hxx
@@ -2,12 +2,15 @@
 #define KANGSW_CPPHEADERS_ARCHIVING_HELPER_MACROS_HXX
 #include "__macro_utility.hxx"
 
-#define INTERNAL_CPPHDRS_ARCHIVING_BRK_TOKENS(STR)                                   \
-    static constexpr char KEYSTR_##__LINE__[] = STR;                                 \
-    static constexpr auto KEYSTR_VIEW_##__LINE__                                     \
-            = CPPHEADERS_NS_::archiving::detail::break_VA_ARGS<KEYSTR_##__LINE__>(); \
-    static const inline auto KEYARRAY_##__LINE__                                     \
-            = CPPHEADERS_NS_::archiving::detail::views_to_strings(KEYSTR_VIEW_##__LINE__);
+#define INTERNAL_CPPHDRS_CONCAT2(A, B) A##B
+#define INTERNAL_CPPHDRS_CONCAT(A, B)  INTERNAL_CPPHDRS_CONCAT2(A, B)
+
+#define INTERNAL_CPPHDRS_ARCHIVING_BRK_TOKENS(STR)                                                            \
+    static constexpr char INTERNAL_CPPHDRS_CONCAT(KEYSTR_, __LINE__)[] = STR;                                 \
+    static constexpr auto INTERNAL_CPPHDRS_CONCAT(KEYSTR_VIEW_, __LINE__)                                     \
+            = CPPHEADERS_NS_::archiving::detail::break_VA_ARGS<INTERNAL_CPPHDRS_CONCAT(KEYSTR_, __LINE__)>(); \
+    static const inline auto INTERNAL_CPPHDRS_CONCAT(KEYARRAY_, __LINE__)                                     \
+            = CPPHEADERS_NS_::archiving::detail::views_to_strings(INTERNAL_CPPHDRS_CONCAT(KEYSTR_VIEW_, __LINE__));
 
 #if __has_include("nlohmann/json.hpp")
 #    include <nlohmann/json.hpp>
@@ -19,7 +22,7 @@
             namespace _ns = CPPHEADERS_NS_::archiving::detail;      \
                                                                     \
             _ns::visit_with_key(                                    \
-                    KEYARRAY_##__LINE__,                            \
+                    INTERNAL_CPPHDRS_CONCAT(KEYARRAY_, __LINE__),   \
                     _ns::from_json_visitor(r),                      \
                     __VA_ARGS__);                                   \
         }                                                           \
@@ -36,7 +39,7 @@
             namespace _ns = CPPHEADERS_NS_::archiving::detail;      \
                                                                     \
             CPPHEADERS_NS_::archiving::detail::visit_with_key(      \
-                    KEYARRAY_##__LINE__,                            \
+                    INTERNAL_CPPHDRS_CONCAT(KEYARRAY_, __LINE__),   \
                     _ns::to_json_visitor(r),                        \
                     __VA_ARGS__);                                   \
         }                                                           \
