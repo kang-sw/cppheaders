e9b9943b683fc0cf88009e15c927fc1c155b39a6
swkang <swkang@pxscope.com>
2021-12-22 10:12:36 +0900

    Resolve compilation error

--- thread/event_wait.hxx
+++ thread/event_wait.hxx
@@ -8,8 +8,8 @@
 namespace CPPHEADERS_NS_::thread {
 using namespace std::literals;
 
-constexpr auto event_wait_default_predicate = [] { return true; };
-constexpr auto event_wait_default_critical  = [] { return true; };
+const auto event_wait_default_predicate = [] { return true; };
+const auto event_wait_default_critical  = [] { return true; };
 
 /**
  * Simple wrapper of event wait handle
@@ -21,12 +21,12 @@ class event_wait
     using cvar_type  = std::condition_variable;
 
     using ulock_type = std::unique_lock<mutex_type>;
-    using pred_type  = bool (*)();
-    using opr_type   = void (*)();
+    using pred_type  = decltype(event_wait_default_predicate);
+    using opr_type   = decltype(event_wait_default_critical);
 
    public:
     template <typename Opr_ = opr_type>
-    void notify_one(Opr_&& critical_proc = event_wait_default_critical)
+    void notify_one(Opr_&& critical_proc)
     {
         ulock_type lc{_mtx};
         critical_proc();
@@ -34,7 +34,7 @@ class event_wait
     }
 
     template <typename Opr_ = opr_type>
-    void notify_all(Opr_&& critical_proc = event_wait_default_critical)
+    void notify_all(Opr_&& critical_proc)
     {
         ulock_type lc{_mtx};
         critical_proc();
@@ -42,38 +42,79 @@ class event_wait
     }
 
     template <typename Opr_ = opr_type>
-    void notify_on_thread_exit(Opr_&& critical_proc = event_wait_default_critical)
+    void notify_all_at_thread_exit(Opr_&& critical_proc)
     {
         ulock_type lc{_mtx};
         critical_proc();
-        notify_all_at_thread_exit(_cvar, std::move(lc));
+        std::notify_all_at_thread_exit(_cvar, std::move(lc));
     }
 
-    template <typename Pred_ = pred_type>
-    void wait(Pred_&& predicate = event_wait_default_predicate)
+    void notify_one()
+    {
+        ulock_type lc{_mtx};
+        _cvar.notify_one();
+    }
+
+    void notify_all()
+    {
+        ulock_type lc{_mtx};
+        _cvar.notify_all();
+    }
+
+    void notify_all_at_thread_exit()
+    {
+        ulock_type lc{_mtx};
+        std::notify_all_at_thread_exit(_cvar, std::move(lc));
+    }
+
+    template <typename Pred_>
+    void wait(Pred_&& predicate) const
     {
         ulock_type lc{_mtx};
         _cvar.wait(lc, std::forward<Pred_>(predicate));
     }
 
-    template <typename Duration_, typename Pred_ = pred_type>
-    bool wait_for(Duration_ duration, Pred_&& predicate = event_wait_default_predicate)
+    void wait() const
     {
         ulock_type lc{_mtx};
-        return _cvar.wait_for(duration, std::forward<Pred_>(predicate));
+        _cvar.wait(lc);
     }
 
-    template <typename TimePoint_, typename Predicate_ = pred_type>
-    bool wait_until(TimePoint_ time_point, Predicate_&& predicate = event_wait_default_predicate)
+    template <typename Duration_, typename Pred_>
+    auto wait_for(Duration_&& duration, Pred_&& predicate) const
     {
         ulock_type lc{_mtx};
-        return _cvar.wait_until(time_point, std::forward<Predicate_>(predicate));
+        return _cvar.wait_for(lc, std::forward<Duration_>(duration), std::forward<Pred_>(predicate));
     }
 
-    auto get_lock() const { return ulock_type{_mtx}; }
+    template <typename Duration_>
+    auto wait_for(Duration_&& duration) const
+    {
+        ulock_type lc{_mtx};
+        return _cvar.wait_for(lc, std::forward<Duration_>(duration));
+    }
+
+    template <typename TimePoint_, typename Predicate_>
+    auto wait_until(TimePoint_&& time_point, Predicate_&& predicate) const
+    {
+        ulock_type lc{_mtx};
+        return _cvar.wait_until(
+                lc,
+                std::forward<TimePoint_>(time_point),
+                std::forward<Predicate_>(predicate));
+    }
+
+    template <typename TimePoint_>
+    auto wait_until(TimePoint_&& time_point) const
+    {
+        ulock_type lc{_mtx};
+        return _cvar.wait_until(lc, std::forward<TimePoint_>(time_point));
+    }
+
+    auto lock() const { return ulock_type{_mtx}; }
 
    private:
-    std::condition_variable _cvar;
+    mutable std::condition_variable _cvar;
     mutable std::mutex _mtx;
 };
 
