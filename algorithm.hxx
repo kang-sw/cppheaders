50fa868bc56bd062f5777f6ac6135bc85d677851
ki6080 <ki6080@gmail.com>
2021-12-25 17:18:41 +0900

    Update copyright

--- algorithm.hxx
+++ algorithm.hxx
@@ -2,7 +2,7 @@
 /*******************************************************************************
  * MIT License
  *
- * Copyright (c) 2021.
+ * Copyright (c) 2021. Seungwoo Kang
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -21,6 +21,8 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
+ *
+ * project home: https://github.com/perfkitpp
  ******************************************************************************/
 
 #pragma once
