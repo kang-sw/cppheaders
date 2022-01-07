ec697f55d9a6031f40bd3ee1f83199a855148a71
ki6080 <ki6080@gmail.com>
2022-01-08 01:07:27 +0900

    Change bool conversion explicit

--- hasher.hxx
+++ hasher.hxx
@@ -100,7 +100,8 @@ struct basic_key
         return self.value < v;
     }
 
-    operator bool() const noexcept { return value != 0; }
+    explicit operator bool() const noexcept { return valid(); }
+    bool valid() const noexcept { return value != 0; }
 
     template <typename Ty_>
     [[deprecated("Use hash() instead")]]  //
