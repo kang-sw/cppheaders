5f56245eeaaa7a1ee2d4d3cc1a5a402ccfcfb1bb
ki6080 <ki6080@gmail.com>
2022-02-10 11:42:04 +0900

    [wip] dequeu

--- container/deque.hxx
+++ container/deque.hxx
@@ -83,7 +83,7 @@ class deque : Alloc_  // zero_size optimization
         _cp_triv     = std::is_trivially_copyable_v<value_type>,
         _dt_triv     = std::is_trivially_destructible_v<value_type>,
 
-        _power = detail::_nearlest_llog2(block_size),
+        _shift = detail::_nearlest_llog2(block_size),
         _mask  = block_size - 1,
     };
 
@@ -93,16 +93,12 @@ class deque : Alloc_  // zero_size optimization
     {
         using container_type = std::conditional_t<IsMutable_, deque, deque const>;
 
-        _block_type** _base;
-        _block_type* _head;
+        deque* _owner;
+        size_t _index;
 
        private:
         void _step_once() noexcept
         {
-            if (++_head - *_base < block_size)
-                ;
-            else
-                ++_base, _head = *_base ? *_base : nullptr;
         }
     };
 
@@ -110,8 +106,7 @@ class deque : Alloc_  // zero_size optimization
     using const_iterator = _basic_iterator<false>;
 
    private:
-    std::vector<_block_type*> _in_use;
-    std::vector<_block_type*> _in_avail;
+    std::vector<_block_type*> _buffers;
 
     size_type _ofst;  // offset from first array element
     size_type _size;
@@ -123,25 +118,104 @@ class deque : Alloc_  // zero_size optimization
 
     void reserve(size_type n_elems)
     {
-        auto n_new_blk = _n_blocks(n_elems) - _n_blocks();
+        auto n_new_blk = _calc_n_blocks(n_elems);
+        auto n_cur_blk = _n_blocks();
+        if (n_new_blk > n_cur_blk) { return; }  // do nothing if already sufficient
+
+        n_new_blk -= n_cur_blk;
+        _buffers.reserve(_buffers.size() + n_new_blk);
+        while (n_new_blk--) { _alloc_once(); }
+    }
+
+    void shrink_to_fit() noexcept
+    {
+    }
+
+    // NOTE: Insertion
+    //  - spin until element size exceeds capacity.
+    //    once exceeds, reallocate pointer buffer and make them in right order.
+    //  - during spin, efficiency between inserting back and front are similar.
+    //    however, expanding front is less efficient than back, as it requires
+    //     rearrangement of buffer pointer array on every buffer growth.
+    template <typename... Args_>
+    reference emplace_back() noexcept(std::is_nothrow_constructible_v<value_type, Args_...>)
+    {
+        _reserve_back(1);
+    }
+
+    template <typename... Args_>
+    reference emplace_front() noexcept(std::is_nothrow_constructible_v<value_type, Args_...>)
+    {
+        _reserve_front(1);
     }
 
    private:
-    constexpr static size_type _div(size_type n) noexcept { return n >> _power; }
-    constexpr static size_type _mod(size_type n) noexcept { return n & _mask; }
+    void _reserve_back(size_t n)
+    {
+        if (_space_head() < n)
+        {
+        }
+    }
+
+    void _reserve_front(size_t n)
+    {
+    }
+
+    void _flatten()
+    {
+        // make deque buffers be flatten
+    }
+
+    bool _is_block_full() const noexcept
+    {
+    }
+
+    size_t _space_head() const noexcept
+    {
+        // remaining space that can be used for inserting elements into head.
+        // (number of fully empty blocks + remining size of current block)
+        auto [min, max] = std::minmax(_blk_tail(), _blk_head());
+        auto n_empty    = (max - min) * block_size;
+
+    }
+
+    size_t _space_tail() const noexcept
+    {
+        // remaining space that can be used for inserting elements into tail
+        // (number of fully empty blocks + remining size of current block)
+        auto cur_block_occupied = block_size - (_ofst & _mask);
+
+        return block_size * (_blk_head_linear() - _blk_tail()) + cur_block_occupied;
+    }
+
+    size_t _blk_tail() const noexcept
+    {
+        return _ofst >> _shift;
+    }
+
+    size_t _blk_head() const noexcept
+    {
+        return _blk_head_linear() % _buffers.size();
+    }
+
+    size_t _blk_head_linear() const noexcept
+    {
+        return ((_ofst + _size) >> _shift);
+    }
 
     size_type _n_blocks() const noexcept
     {
-        return (_in_use.empty() ? 0 : _in_use.size() - 1) + _in_avail.size();
+        return _buffers.size();
     }
 
-    size_type _n_blocks(size_type n_elems) const noexcept
+    static size_type _calc_n_blocks(size_type n_elems) noexcept
     {
+        return (n_elems + _mask) >> _shift;
     }
 
-    void _alloc_once(size_type n_total)
+    void _alloc_once()
     {
-        _in_avail.push_back(reinterpret_cast<_block_type*>(this->allocate(block_size)));
+        _buffers.push_back(reinterpret_cast<_block_type*>(this->allocate(block_size)));
     }
 };
 
