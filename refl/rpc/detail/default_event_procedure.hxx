6d32d247013471ebb029a7f16ec88deb41426bf1
ki6080 <ki6080@gmail.com>
2022-04-06 15:02:09 +0900

    rpc.session: Make concurrency configurable

--- refl/rpc/detail/default_event_procedure.hxx
+++ refl/rpc/detail/default_event_procedure.hxx
@@ -32,26 +32,36 @@
 namespace CPPHEADERS_NS_::rpc {
 class default_event_procedure : public if_event_proc
 {
+    thread_pool* _tpool = nullptr;
+
    public:
     void post_rpc_completion(function<void()>&& fn) override
     {
-        default_singleton<thread_pool>().post(std::move(fn));
+        _tpool->post(std::move(fn));
     }
 
     void post_handler_callback(function<void()>&& fn) override
     {
-        default_singleton<thread_pool>().post(std::move(fn));
+        _tpool->post(std::move(fn));
     }
 
     void post_internal_message(function<void()>&& fn) override
     {
-        default_singleton<thread_pool>().post(std::move(fn));
+        _tpool->post(std::move(fn));
     }
 
    public:
     static auto get() noexcept
     {
-        static auto _value = std::make_shared<default_event_procedure>();
+        static auto _value = [] {
+            auto instance = std::make_shared<default_event_procedure>();
+            auto tpool = singleton<thread_pool, default_event_procedure>;
+            tpool.create(1);
+
+            instance->_tpool = &tpool.get();
+            return instance;
+        }();
+
         return _value;
     }
 };
