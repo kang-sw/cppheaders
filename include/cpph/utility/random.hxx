ecfdc9720ca3d945914d942c0806c8dd6f24655f
ki6080 <ki6080@gmail.com>
2022-04-24 19:36:36 +0900

    cpph: Misc file changes

---
+++ include/cpph/utility/random.hxx
@@ -0,0 +1,22 @@
+#pragma once
+#include <random>
+
+#include "cpph/algorithm/std.hxx"
+#include "cpph/template_utils.hxx"
+
+namespace cpph {
+template <typename OutIter, typename Generator>
+OutIter generate_random_characters(OutIter o, size_t n, Generator&& gen)
+{
+    auto constexpr charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"sv;
+    std::uniform_int_distribution<size_t> distr{0, size(charset) - 1};
+    while (n--) { *o++ = distr(gen); }
+    return o;
+}
+
+template <typename OutIter>
+OutIter generate_random_characters(OutIter o, size_t n)
+{
+    return generate_random_characters(o, n, std::mt19937_64{std::random_device{}()});
+}
+}  // namespace cpph
