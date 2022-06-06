05e665e898e84def66d1d66b3c20c84852f9782c
ki6080 <ki6080@gmail.com>
2022-06-06 12:22:27 +0900

    refl: Rename macro

--- include/cpph/refl/detail/object_macros.hxx
+++ include/cpph/refl/detail/object_macros.hxx
@@ -36,7 +36,7 @@
 #    define CPPH_REFL_DEFINE_OBJECT_inline(AttrOps, ...) \
         INTERNAL_CPPH_REFL_EMBED_DEFINE_IMPL(, AttrOps, define_object, INTERNAL_CPPH_REFL_ITERATE_OJBECT_VAR, __VA_ARGS__)
 
-#    define CPPH_REFL_DEFINE_OBJECT_inline_2(...) \
+#    define CPPH_REFL_DEFINE_OBJECT_inline_simple(...) \
         INTERNAL_CPPH_REFL_EMBED_DEFINE_IMPL(, (), define_object, INTERNAL_CPPH_REFL_ITERATE_OJBECT_VAR_2, __VA_ARGS__)
 
 #    define CPPH_REFL_DEFINE_TUPLE_c(ClassName, AttrOps, ...) \
