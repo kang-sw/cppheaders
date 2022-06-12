2192059040e0ec5096cd0d3e1363a9ce5aa2989f
ki6080 <ki6080@gmail.com>
2022-06-12 18:33:08 +0900

    memory.pool: Replace legacy pool

--- include/cpph/memory/pool.hxx
+++ include/cpph/memory/pool.hxx
@@ -39,10 +39,8 @@ using std::vector;
 
 namespace _tr {
 namespace _detail {
-class pool_base;
-
 struct pool_node {
-    weak_ptr<pool_base> owner = {};
+    weak_ptr<class pool_base> owner = {};
     pool_node* next = {};
     char data[];
 
@@ -50,7 +48,7 @@ struct pool_node {
     template <typename T>
     static void dispose(pool_node* n)
     {
-        static_cast<T*>(n->data)->~T();
+        reinterpret_cast<T*>(n->data)->~T();
         dispose_header(n);
     }
 
@@ -68,7 +66,7 @@ struct pool_node {
     }
 
     template <typename T>
-    T* get() const noexcept
+    T* get() noexcept
     {
         return reinterpret_cast<T*>(data);
     }
@@ -99,12 +97,12 @@ class pool_mutex : public if_pool_mutex
     }
 };
 
-class pool_base : enable_shared_from_this<pool_base>
+class pool_base : public std::enable_shared_from_this<pool_base>
 {
     pool_node* _idle_nodes = nullptr;
 
     ptr<if_pool_mutex> _mtx;
-    void (*_dtor)(void*);
+    void (*_dtor)(void*) noexcept;
 
    public:
     pool_base(ptr<if_pool_mutex> mtx, decltype(_dtor) dtor) noexcept : _mtx(move(mtx)), _dtor(dtor)
@@ -113,7 +111,7 @@ class pool_base : enable_shared_from_this<pool_base>
         assert(_dtor);
     }
 
-    ~pool_base()
+    ~pool_base() noexcept
     {
         clear_all_idle();
     }
@@ -122,7 +120,7 @@ class pool_base : enable_shared_from_this<pool_base>
     pool_node* try_checkout() noexcept
     {
         assert(_mtx != nullptr);
-        CPPH_TMPVAR = lock_guard{*_mtx};
+        lock_guard _{*_mtx};
 
         if (not _idle_nodes)
             return nullptr;
@@ -157,7 +155,7 @@ class pool_base : enable_shared_from_this<pool_base>
     {
         assert(n->next == nullptr);
         assert(_mtx != nullptr);
-        CPPH_TMPVAR = lock_guard{*_mtx};
+        lock_guard _{*_mtx};
 
         if (_idle_nodes) {
             n->next = exchange(_idle_nodes, n);
@@ -168,13 +166,15 @@ class pool_base : enable_shared_from_this<pool_base>
         n = nullptr;
     }
 
-    void clear_all_idle()
+    void clear_all_idle() noexcept
     {
         pool_node* node = nullptr;
         {
-            CPPH_TMPVAR = lock_guard{*_mtx};
+            lock_guard _{*_mtx};
             node = exchange(_idle_nodes, nullptr);
         }
+
+        list<std::exception_ptr> exceptions;
 
         for (pool_node* next; node; node = next) {
             next = node->next;
@@ -199,7 +199,7 @@ class pool_ptr
    public:
     pool_ptr() noexcept = default;
     pool_ptr(pool_ptr&& other) noexcept : _node(exchange(other._node, nullptr)) {}
-    pool_ptr& operator=(pool_ptr&& other) noexcept { swap(_node, other._node); }
+    pool_ptr& operator=(pool_ptr&& other) noexcept { return swap(_node, other._node), *this; }
     explicit pool_ptr(_detail::pool_node* h) : _node(h) {}
 
     void checkin() noexcept
@@ -257,33 +257,26 @@ struct _pool_ptr_disposer {
 template <typename T>
 using unique_pool_ptr = unique_ptr<pool_ptr<T>, _pool_ptr_disposer<T>>;
 
-template <typename T, class Mutex>
-class basic_resource_pool
+template <typename T, class Mutex = spinlock, typename... Params>
+class pool : private tuple<Params...>
 {
-    shared_ptr<_detail::pool_base> _pool = make_shared<_detail::pool_base>(
-            make_unique<_detail::pool_mutex<Mutex>>(), &basic_resource_pool::_dtor);
-
-    function<_detail::pool_node*()> _construct_new_node = [this] {
-        return _pool->construct<T>();
-    };
+    shared_ptr<_detail::pool_base> _pool
+            = make_shared<_detail::pool_base>(
+                    make_unique<_detail::pool_mutex<Mutex>>(), &pool::_dtor);
 
    public:
-    basic_resource_pool() noexcept = default;
+    pool() noexcept = default;
+    pool(pool&&) noexcept = default;
+    pool& operator=(pool&&) noexcept = default;
 
-    template <typename Param0, typename... Params>
-    explicit basic_resource_pool(Param0&& p0, Params&&... params)
-            : _construct_new_node([this, params = make_tuple(std::forward<Param0>(p0), std::forward<Params>(params)...)] {
-                  apply(
-                          [this](auto&&... params) {
-                              return _pool->template construct<T>(params...);
-                          },
-                          params);
-              })
+    template <typename Param0, typename... Param1>
+    explicit pool(Param0&& param0, Param1&&... params)
+            : tuple<Params...>(make_tuple(std::forward<Param0>(param0), std::forward<Param1>(params)...))
     {
     }
 
    private:
-    static void _dtor(void* p)
+    static void _dtor(void* p) noexcept
     {
         reinterpret_cast<T*>(p)->~T();
     }
@@ -295,9 +288,13 @@ class basic_resource_pool
     handle_type checkout()
     {
         if (auto node = _pool->try_checkout()) {
-            return {node};
+            return handle_type{node};
         } else {
-            return {_construct_new_node()};
+            return handle_type{std::apply(
+                    [this](auto&&... params) {
+                        return _pool->template construct<T>(params...);
+                    },
+                    static_cast<tuple<Params...>&>(*this))};
         }
     }
 
@@ -313,189 +310,7 @@ class basic_resource_pool
 };
 }  // namespace _tr
 
-template <typename Ty_, typename Mutex_ = null_mutex>
-class basic_resource_pool
-{
-   public:
-    using buffer_type = list<Ty_>;
-    using buffer_iterator = typename buffer_type::iterator;
-
-   public:
-    struct handle_type {
-        class deleter
-        {
-            handle_type _handle;
-
-           public:
-            using pointer = Ty_*;
-
-            deleter(handle_type&& h) noexcept : _handle(move(h)) {}
-            void operator()(pointer) { _handle.checkin(); }
-        };
-
-       public:
-        handle_type() noexcept = default;
-
-        handle_type(handle_type&& r) noexcept
-        {
-            _assign(move(r));
-        }
-
-        handle_type& operator=(handle_type&& r) noexcept
-        {
-            this->~handle_type();
-            _assign(move(r));
-            return *this;
-        }
-
-        ~handle_type() noexcept
-        {
-            valid() && (checkin(), 0);
-        }
-
-        void checkin()
-        {
-            _owner->checkin(move(*this));
-        }
-
-        bool valid() const noexcept
-        {
-            return _owner;
-        }
-
-        Ty_* operator->() const noexcept
-        {
-            return &*_ref;
-        }
-
-        Ty_& operator*() const noexcept
-        {
-            return *_ref;
-        }
-
-        auto get() const noexcept
-        {
-            return &*_ref;
-        }
-
-        auto share() && noexcept
-        {
-            // Backup pointer before move 'this' reference
-            auto pointer = &**this;
-
-            return shared_ptr<Ty_>{
-                    pointer, deleter{move(*this)}};
-        }
-
-        auto unique() && noexcept
-        {
-            auto pointer = &**this;
-
-            return unique_ptr<Ty_, deleter>{
-                    pointer, deleter{move(*this)}};
-        }
-
-       private:
-        void _assign(handle_type&& other)
-        {
-            _owner = other._owner;
-            _ref = other._ref;
-            other._owner = nullptr;
-            other._ref = {};
-        }
-
-       private:
-        friend class basic_resource_pool;
-
-        basic_resource_pool* _owner = nullptr;
-        typename list<Ty_>::iterator _ref{};
-    };
-
-   public:
-    handle_type checkout()
-    {
-        lock_guard _{_mut};
-        handle_type r;
-        r._owner = this;
-
-        if (_free.empty())
-            _fn_construct_back();
-
-        r._ref = _free.begin();
-        _pool.splice(_pool.end(), _free, r._ref);
-        return r;
-    }
-
-    void checkin(handle_type h)
-    {
-        if (not h.valid())
-            throw std::invalid_argument{"invalid handle"};
-
-        if (h._owner != this)
-            throw std::invalid_argument{"invalid owner reference"};
-
-        lock_guard _{_mut};
-        _free.splice(_free.begin(), _pool, h._ref);
-
-        h._owner = {};
-        h._ref = {};
-    }
-
-    void shrink()
-    {
-        lock_guard _{_mut};
-        _free.clear();
-    }
-
-    //! Check if not any resource is currently checked out.
-    bool empty()
-    {
-        lock_guard _{_mut};
-        return _pool.empty();
-    }
-
-    basic_resource_pool()
-    {
-        _fn_construct_back =
-                [this] {
-                    _free.emplace_back();
-                };
-    }
-
-    ~basic_resource_pool()
-    {
-        // TODO: way to detect deadlock? (e.g. from destruction order in same thread ...)
-
-        // Yield for long time, since if following empty() call returns true, it normally
-        //  indicates async process is running from another thread,
-        while (not empty()) { _detail::thread_yield(); }
-    }
-
-    template <typename... Args_>
-    explicit basic_resource_pool(Args_&&... args)
-    {
-        _fn_construct_back =
-                [this, tup_args = forward_as_tuple(args...)] {
-                    auto fn_apply =
-                            [this](auto&&... args) {
-                                _free.emplace_back(forward<decltype(args)>(args)...);
-                            };
-                    apply(fn_apply, tup_args);
-                };
-    }
-
-   private:
-    Mutex_ _mut;
-
-    function<void()> _fn_construct_back;
-    list<Ty_> _pool;
-    list<Ty_> _free;
-};
-
-template <typename Ty_>
-using pool = basic_resource_pool<Ty_, spinlock>;
-
-template <typename Ty_>
-using pool_ptr = typename pool<Ty_>::handle_type;
-
+using _tr::pool;
+using _tr::pool_ptr;
+using _tr::unique_pool_ptr;
 }  // namespace cpph
